6a56e429cbb77c846b319e30d762fac9b5d246fa
Zeale <Zeale@dusttoash.org>
2018-07-24 20:29:36 -0500

    Updated Evaluator

--- src/org/alixia/libs/evaluator/Evaluator.java
+++ src/org/alixia/libs/evaluator/Evaluator.java
@@ -1,7 +1,21 @@
 package org.alixia.libs.evaluator;
 
-public final class Evaluator {
+import org.alixia.libs.evaluator.api.Spate;
 
-	private Evaluator() {	}
+public class Evaluator<T extends Number> {
+
+	private Evaluator() {
+	}
+
+	private Spate<Character> equation;
+
+	public synchronized double solve(Spate<Character> equation) {
+		this.equation = equation;
+		// TODO Check pre-solved value if it exists (or maybe don't; Spate may have
+		// changed).
+		while (equation.hasNext())
+			;
+		return 0;
+	}
 
 }
