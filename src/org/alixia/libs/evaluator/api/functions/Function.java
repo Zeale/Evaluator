befa1d65ba37f8fcedb209d35f4c707432a73cbe
Zeale <Zeale@dusttoash.org>
2018-07-28 19:14:12 -0500

    Updated Function class

--- src/org/alixia/libs/evaluator/api/functions/Function.java
+++ src/org/alixia/libs/evaluator/api/functions/Function.java
@@ -1,21 +1,22 @@
 package org.alixia.libs.evaluator.api.functions;
 
 import org.alixia.libs.evaluator.api.VariadicFunction;
+import org.alixia.libs.evaluator.api.terms.Number;
 
-public abstract class Function<T, AST> {
+public class Function<T, AST> {
 	private final String name;
 	private final boolean ignoreCase;
-	private final Class<? extends AST>[] args;
-	private final Class<T> returnType;
+
+	public static final Number<Double> sine(double input) {
+		return new Number<Double>(Math.sin(input));
+	}
+
 
 	private final VariadicFunction<AST, T> function;
 
-	public Function(String name, boolean ignoreCase, Class<? extends AST>[] args, Class<T> returnType,
-			VariadicFunction<AST, T> function) {
+	public Function(String name, boolean ignoreCase, VariadicFunction<AST, T> function) {
 		this.name = name;
 		this.ignoreCase = ignoreCase;
-		this.args = args;
-		this.returnType = returnType;
 		this.function = function;
 	}
 
