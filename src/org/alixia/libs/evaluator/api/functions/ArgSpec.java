57389ec9902ca02068b15d02625962f94ea46374
Zeale <Zeale@dusttoash.org>
2018-07-28 19:16:25 -0500

    Added argument and function specs

---
+++ src/org/alixia/libs/evaluator/api/functions/ArgSpec.java
@@ -0,0 +1,16 @@
+package org.alixia.libs.evaluator.api.functions;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(PARAMETER)
+public @Documented @interface ArgSpec {
+	boolean necessary() default true;
+
+	Class<?> validInputs() default void.class;
+}
