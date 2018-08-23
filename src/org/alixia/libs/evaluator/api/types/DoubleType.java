db60f8d02f573f74a24754c1e2f2640de24f7b45
Zeale <zeale@dusttoash.org>
2018-08-22 23:40:18 -0500

    Added the DoubleType class

---
+++ src/org/alixia/libs/evaluator/api/types/DoubleType.java
@@ -0,0 +1,8 @@
+package org.alixia.libs.evaluator.api.types;
+
+public class DoubleType implements Type<Double> {
+	public static final DoubleType INSTANCE = new DoubleType();
+
+	private DoubleType() {
+	}
+}
