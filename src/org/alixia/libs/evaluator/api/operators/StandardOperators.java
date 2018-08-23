c79a27869a404a34efc3559ffe8ec679c3e5fc81
Zeale <zeale@dusttoash.org>
2018-08-23 10:12:48 -0500

    Updated StandardOperators

--- src/org/alixia/libs/evaluator/api/operators/StandardOperators.java
+++ src/org/alixia/libs/evaluator/api/operators/StandardOperators.java
@@ -3,31 +3,42 @@ package org.alixia.libs.evaluator.api.operators;
 import java.math.BigDecimal;
 import java.util.function.BiFunction;
 
-import org.alixia.libs.evaluator.api.terms.Number;
 import org.alixia.libs.evaluator.api.terms.Term;
+import org.alixia.libs.evaluator.api.types.Data;
+import org.alixia.libs.evaluator.api.types.NumericData;
 
 public enum StandardOperators implements NormalOperator, Precedented {
-	ADD((a, b) -> a.add(b), 1), SUBTRACT((a, b) -> a.subtract(b), 1), MULTIPLY((a, b) -> a.multiply(b), 2),
-	DIVIDE((a, b) -> a.divide(b), 2), EXPONENTIATION((a, b) -> a.pow(b.intValue()), 3),
-	MODULUS((a, b) -> a.remainder(b), 2);
+	ADD((BigDecimalHandler) BigDecimal::add, 1), SUBTRACT((BigDecimalHandler) BigDecimal::subtract, 1),
+	MULTIPLY((BigDecimalHandler) BigDecimal::multiply, 2), DIVIDE((BigDecimalHandler) BigDecimal::divide, 2),
+	EXPONENTIATION((BigDecimalHandler) (a, b) -> a.pow(b.intValue()), 3),
+	MODULUS((BigDecimalHandler) BigDecimal::remainder, 2);
 
-	private final BiFunction<?, ?, ?> function;
+	private final BiFunction<Data<?>, Data<?>, Data<?>> function;
 
 	private final Precedence precedence;
 
-	private StandardOperators(final BiFunction<BigDecimal, BigDecimal, BigDecimal> function, final int precedence) {
+	private StandardOperators(final BiFunction<Data<?>, Data<?>, Data<?>> function, final int precedence) {
 		this.function = function;
 		this.precedence = new Precedence(precedence);
 	}
 
 	@Override
-	public Term<BigDecimal> evaluate(final Term<?> first, final Term<?> second) {
-		return new Number<>(function.apply(first.evaluate(), second.evaluate()));
+	public Term<?> evaluate(final Term<? extends Data<?>> first, final Term<? extends Data<?>> second) {
+		return Term.wrap(function.apply(first.evaluate(), second.evaluate()));
 	}
 
 	@Override
 	public Precedence precedence() {
 		return precedence;
+	}
+
+	private interface BigDecimalHandler extends BiFunction<Data<?>, Data<?>, Data<?>> {
+		@Override
+		default Data<?> apply(Data<?> t, Data<?> u) {
+			return new NumericData(apply(t.toNumericData().evaluate(), u.toNumericData().evaluate()));
+		}
+
+		BigDecimal apply(BigDecimal first, BigDecimal second);
 	}
 
 }
