92351b9375c57d9f6ce6bf2a1c98a9d6f99db879
Zeale <zeale@dusttoash.org>
2018-08-25 18:23:33 -0500

    Updated the DefaultTypeMap

--- src/org/alixia/libs/evaluator/api/types/casting/DefaultTypeMap.java
+++ src/org/alixia/libs/evaluator/api/types/casting/DefaultTypeMap.java
@@ -1,5 +1,7 @@
 package org.alixia.libs.evaluator.api.types.casting;
 
+import java.lang.reflect.Modifier;
+
 import org.alixia.libs.evaluator.api.types.Data;
 
 public interface DefaultTypeMap extends TypeMap {
@@ -11,7 +13,8 @@ public interface DefaultTypeMap extends TypeMap {
 		String absoluteName = ref.contains(".") ? ref : Data.class.getPackage().getName() + "." + ref;
 		try {
 			Class<?> resolvedClass = Class.forName(absoluteName);
-			if (!Data.class.isAssignableFrom(resolvedClass))
+			if (!Data.class.isAssignableFrom(resolvedClass) || Modifier.isAbstract(resolvedClass.getModifiers())
+					|| Modifier.isInterface(resolvedClass.getModifiers()))
 				throw new RuntimeException("The specified class is not a viable Data type.");
 			else
 				return (Class<? extends Data<?>>) resolvedClass;
@@ -19,4 +22,27 @@ public interface DefaultTypeMap extends TypeMap {
 			return null;
 		}
 	}
+
+	static String filter(String ref) {
+		String[] pieces = ref.split("\\.");
+		String name = pieces[pieces.length - 1];
+		name = name.replaceAll(" ", "-");
+		name = name.replaceAll("_", "-");
+		while (name.contains("--"))
+			name = name.replaceAll("--", "-");
+
+		String[] words = name.split("-");
+		for (int i = 0; i < words.length; i++)
+			words[i] = words[i].substring(0, 1).toUpperCase() + (words[i].length() > 1 ? words[i].substring(1) : "");
+		String finalizedName = "";
+		for (String s : words)
+			finalizedName += s;
+		String absolutePath = "";
+		for (int i = 0; i < pieces.length - 1; i++) {
+			String s = pieces[i];
+			absolutePath += s + ".";
+		}
+		return absolutePath + finalizedName;// We append this out here to make sure that no trailing period is appended.
+	}
+
 }
