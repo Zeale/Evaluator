04a7ec553778892486feda447205220c88893e65
Zeale <zeale@dusttoash.org>
2018-08-25 18:08:06 -0500

    Documented TypeMap

--- src/org/alixia/libs/evaluator/api/types/casting/TypeMap.java
+++ src/org/alixia/libs/evaluator/api/types/casting/TypeMap.java
@@ -1,5 +1,17 @@
 package org.alixia.libs.evaluator.api.types.casting;
 
+import org.alixia.libs.evaluator.api.types.Data;
+
 public interface TypeMap {
-	Class<?> get(String ref);
+	/**
+	 * Returns a {@link Class} representing the specified ref. The class object will
+	 * be a subclass of {@link Data} or {@link Data} itself.
+	 * 
+	 * @param ref The text that should reference the wanted type in some way.
+	 * @return This method makes its best attempt to figure out what
+	 *         <code>ref</code> refers to. If this method figures out what
+	 *         <code>ref</code> refers to, it will return it. Otherwise, it will
+	 *         return <code>null</code>.
+	 */
+	Class<? extends Data<?>> get(String ref);
 }
