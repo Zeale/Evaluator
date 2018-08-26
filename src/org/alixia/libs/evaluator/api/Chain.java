0f0ea50a5f76c49291ba6d9abc9e7c67a3340c99
Zeale <zeale@dusttoash.org>
2018-08-26 14:20:00 -0500

    Added the nested type Pair to Chain

--- src/org/alixia/libs/evaluator/api/Chain.java
+++ src/org/alixia/libs/evaluator/api/Chain.java
@@ -2,4 +2,30 @@ package org.alixia.libs.evaluator.api;
 
 public class Chain<F, S> {
 
+	public final class Pair {
+
+		private boolean last;
+
+		public boolean isLast() {
+			return last;
+		}
+
+		private final F first;
+		private final S second;
+
+		public F getFirst() {
+			return first;
+		}
+
+		public S getSecond() {
+			return second;
+		}
+
+		private Pair(F first, S second) {
+			this.first = first;
+			this.second = second;
+		}
+
+	}
+
 }
