73d27290f16771bcd8ea31bb5d241b84cf0d128d
Zeale <zeale@dusttoash.org>
2018-08-26 17:00:14 -0500

    Updated SimpleOperator interface

--- src/org/alixia/libs/evaluator/api/operators/SimpleOperator.java
+++ src/org/alixia/libs/evaluator/api/operators/SimpleOperator.java
@@ -1,10 +1,21 @@
 package org.alixia.libs.evaluator.api.operators;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import org.alixia.libs.evaluator.api.terms.ChainTerm;
 import org.alixia.libs.evaluator.api.terms.Term;
 import org.alixia.libs.evaluator.api.types.Data;
+import org.alixia.libs.evaluator.api.types.NumericData;
 
 public interface SimpleOperator<R extends Data<?>> extends Operator {
+
+	public final static SimpleOperator<NumericData> FACTORIAL = inputTerm -> {
+		BigInteger result = BigInteger.ONE, input = inputTerm.evaluate().toNumericData().evaluate().toBigInteger();
+		for (BigInteger i = BigInteger.ONE; i.compareTo(input) < 1; i = i.add(BigInteger.ONE))
+			result = result.multiply(i);
+		return Term.wrap(new NumericData(new BigDecimal(result)));
+	};
 
 	public Term<R> operate(Term<?> inputTerm);
 
