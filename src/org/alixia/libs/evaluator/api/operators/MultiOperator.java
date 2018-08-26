be94e1713ebb165aef27a4cf498c284643ff9999
Zeale <zeale@dusttoash.org>
2018-08-26 17:04:30 -0500

    Added a MultiOperator type

---
+++ src/org/alixia/libs/evaluator/api/operators/MultiOperator.java
@@ -0,0 +1,25 @@
+package org.alixia.libs.evaluator.api.operators;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.alixia.libs.evaluator.api.terms.ChainTerm;
+
+public class MultiOperator implements Operator {
+	private final NormalOperator combiningOperator;
+	private final List<Operator> otherOperators = new LinkedList<>();
+
+	public MultiOperator(NormalOperator combiningOperator, Operator... otherOperators) {
+		this.combiningOperator = combiningOperator;
+		for (Operator o : otherOperators)
+			this.otherOperators.add(o);
+	}
+
+	@Override
+	public void evaluate(ChainTerm<?>.MathChain chain, ChainTerm<?>.MathChain.MathIterator iterator) {
+		for (Operator o : otherOperators)
+			o.evaluate(chain, iterator);
+		combiningOperator.evaluate(chain, iterator);
+	}
+
+}
