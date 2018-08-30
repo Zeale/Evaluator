541d73f702ba876561aa24d3a3eb335510a4d5c3
Zeale <zeale@dusttoash.org>
2018-08-30 07:53:09 -0500

    Added the base exception class

---
+++ src/org/alixia/libs/evaluator/EvaluationException.java
@@ -0,0 +1,27 @@
+package org.alixia.libs.evaluator;
+
+public abstract class EvaluationException extends RuntimeException {
+
+	/**
+	 * SUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public final String generalLocation;
+
+	public EvaluationException(String message, String generalLocation) {
+		super(message);
+		this.generalLocation = generalLocation;
+	}
+
+	public EvaluationException(String message, Throwable cause, String generalLocation) {
+		super(message, cause);
+		this.generalLocation = generalLocation;
+	}
+
+	public EvaluationException(String message, Throwable cause) {
+		super(message, cause);
+		generalLocation = null;
+	}
+
+}
